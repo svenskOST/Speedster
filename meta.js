c0fba531f50a2ea72dc86e896cf90c62e35d7b02
Alexander Marini <svenskost6@gmail.com>
2022-10-21 18:13:50 +0200

    created respitory

---
+++ meta.js
@@ -0,0 +1,135 @@
+const Bugatti = [Chiron, Veyron]
+const Ferrari = [Daytona_SP3, LaFerrari]
+const Volvo = [V90]
+
+const Chiron = function(){
+    var topspeed = 490.484
+    var gear = [g1, g2, g3, g4, g5, g6, g7]
+    var acceleration 
+    function g1(){
+        acceleration = temp
+    }
+    function g2(){
+        acceleration = temp
+    }
+    function g3(){
+        acceleration = temp
+    }
+    function g4(){
+        acceleration = temp
+    }
+    function g5(){
+        acceleration = temp
+    }
+    function g6(){
+        acceleration = temp
+    }
+    function g7(){
+        acceleration = temp
+    }
+}
+
+const Veyron = function(){
+    var topspeed = temp
+    var gear = [g1, g2, g3, g4, g5, g6, g7]
+    var acceleration 
+    function g1(){
+        acceleration = temp
+    }
+    function g2(){
+        acceleration = temp
+    }
+    function g3(){
+        acceleration = temp
+    }
+    function g4(){
+        acceleration = temp
+    }
+    function g5(){
+        acceleration = temp
+    }
+    function g6(){
+        acceleration = temp
+    }
+    function g7(){
+        acceleration = temp
+    }
+}
+
+const Daytona_SP3 = function(){
+    var topspeed = temp
+    var gear = [g1, g2, g3, g4, g5, g6, g7]
+    var acceleration 
+    function g1(){
+        acceleration = temp
+    }
+    function g2(){
+        acceleration = temp
+    }
+    function g3(){
+        acceleration = temp
+    }
+    function g4(){
+        acceleration = temp
+    }
+    function g5(){
+        acceleration = temp
+    }
+    function g6(){
+        acceleration = temp
+    }
+    function g7(){
+        acceleration = temp
+    }
+}
+
+const LaFerrari = function(){
+    var topspeed = temp
+    var gear = [g1, g2, g3, g4, g5, g6, g7]
+    var acceleration 
+    function g1(){
+        acceleration = temp
+    }
+    function g2(){
+        acceleration = temp
+    }
+    function g3(){
+        acceleration = temp
+    }
+    function g4(){
+        acceleration = temp
+    }
+    function g5(){
+        acceleration = temp
+    }
+    function g6(){
+        acceleration = temp
+    }
+    function g7(){
+        acceleration = temp
+    }
+}
+
+const V90 = function(){
+    var topspeed = temp
+    var gear = [g1, g2, g3, g4, g5, g6]
+    var acceleration 
+    function g1(){
+        acceleration = temp
+    }
+    function g2(){
+        acceleration = temp
+    }
+    function g3(){
+        acceleration = temp
+    }
+    function g4(){
+        acceleration = temp
+    }
+    function g5(){
+        acceleration = temp
+    }
+    function g6(){
+        acceleration = temp
+    }
+}
